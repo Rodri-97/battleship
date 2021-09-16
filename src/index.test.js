ef33cbca3154ff07705ec2f2aa5a843d0cc6d837
rodrigo <rodrigo_coelho@orange.fr>
2021-09-16 17:58:47 +0200

    additional test

--- src/index.test.js
+++ src/index.test.js
@@ -10,12 +10,15 @@ test("Ship not sunk by default", () => {
   expect(newShip.isSunk()).toBe(false);
 });
 
-test("Sink ship", () => {
+test("Ship not sunk after hitting some of the targets", () => {
   newShip.hit(0);
   newShip.hit(1);
   newShip.hit(2);
   newShip.hit(3);
+  expect(newShip.isSunk()).toBe(false);
+});
+
+test("Ship sunk after hitting all of the targets", () => {
   newShip.hit(4);
   expect(newShip.isSunk()).toBe(true);
 });
-
