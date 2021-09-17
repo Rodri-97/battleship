6d9c0d730338017f25d0bb388de91ddc8bc43745
rodrigo <rodrigo_coelho@orange.fr>
2021-09-17 17:30:13 +0200

    edit gameboard lines for loop

--- src/factories/gameboard.js
+++ src/factories/gameboard.js
@@ -2,7 +2,7 @@ const Ship = require("./ship.js");
 
 const Gameboard = (gridSize) => {
     const lines = new Array(gridSize).fill([]);
-    for (let line of lines) line.push("");
+    for (let i = 0; i < lines.length; i++) lines[i].push("");
 
     return { lines };
 };
